2224563707062ca99233c2f389e9ac29d1a43d37
Deivz <carlosd_oliveira@hotmail.com>
2025-02-07 16:08:48 -0300

    Commit das rotas iniciais do projeto

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,12 @@
 <?php
 
 use Deivz\CalculadoraIr\controllers\Cadastro;
-use Deivz\CalculadoraIr\controllers\ListaOperacoes;
-use Deivz\CalculadoraIr\controllers\Login;
-use Deivz\CalculadoraIr\controllers\Logout;
-use Deivz\CalculadoraIr\controllers\Operacoes;
+
 
 $rotas = [
-    '' => Login::class,
-    '/cadastro' => Cadastro::class,
-    '/login' => Login::class,
-    '/operacoes' => Operacoes::class,
-    '/logout' => Logout::class,
-    '/operacoes/lista' => ListaOperacoes::class
+    '/cadastro-tipos' => Cadastro::class,
+    '/cadastro-produtos' => Cadastro::class,
+    '/cadastro-impostos' => Cadastro::class,
 ];
 
 return $rotas;
