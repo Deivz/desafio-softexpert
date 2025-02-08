8085467170613f4a535e0d3fc59a7ef7bb599afa
Deivz <carlosd_oliveira@hotmail.com>
2025-02-08 13:43:03 -0300

    Rota de Imposto implementada

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ $routes = [
 	'GET' => [
 		'/cadastro-produto' => 'ProductController@read',
 		'/cadastro-tipo-produto' => 'ProductTypeController@read',
+		'/cadastro-imposto' => 'TaxController@read',
 		// '/' => 'HomeController@index',
 		// '/about' => 'AboutController@index',
 		// '/user/{id}' => 'UserController@show', // Rota dinâmica
@@ -12,14 +13,17 @@ $routes = [
 	'POST' => [
 		'/cadastro-produto' => 'ProductController@create',
 		'/cadastro-tipo-produto' => 'ProductTypeController@create',
+		'/cadastro-imposto' => 'TaxController@create',
 	],
 	'PATCH' => [
 		'/cadastro-produto' => 'ProductController@update',
 		'/cadastro-tipo-produto' => 'ProductTypeController@update',
+		'/cadastro-imposto' => 'TaxController@update',
 	],
 	'DELETE' => [
 		'/cadastro-produto' => 'ProductController@delete',
 		'/cadastro-tipo-produto' => 'ProductTypeController@delete',
+		'/cadastro-imposto' => 'TaxController@delete',
 	],
 ];
 
