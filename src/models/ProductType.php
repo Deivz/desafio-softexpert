fc017cb35f8d556462f38829d1edf6571fcb1a60
Deivz <carlosd_oliveira@hotmail.com>
2025-02-09 15:57:34 -0300

    Model ProductType implementado seguindo novo pattern

--- src/models/ProductType.php
+++ src/models/ProductType.php
@@ -2,96 +2,72 @@
 
 namespace Deivz\DesafioSoftexpert\models;
 
-use Deivz\DesafioSoftexpert\controllers\ConnectionController;
 use Deivz\DesafioSoftexpert\helpers\UUIDGenerator;
-use Deivz\DesafioSoftexpert\interfaces\ModelInterface;
-use PDO;
+use Deivz\DesafioSoftexpert\helpers\Validator;
 
-use function Deivz\DesafioSoftexpert\helpers\uuidv4;
-
-class ProductType implements ModelInterface
+class ProductType
 {
-  private ConnectionController $connectionController;
-  private PDO $connection;
-  private string $table;
-  private array $columns = [];
+	private string $uuid;
+	private int $deleted;
+	private int $active;
+	private string $productType;
+	private string $createdAt;
+	private string $updatedAt;
 
-  public function __construct(ConnectionController $connectionController)
-  {
-    $this->connectionController = $connectionController;
-    $this->table = $_ENV["TABLE_PRODUCT_TYPES"];
-    $this->columns = [
-      'id',
-      'uuid',
-      'deleted',
-      'active',
-      'product_type',
-      'created_at',
-      'updated_at',
-    ];
-  }
-
-  public function __get($attribute)
-  {
-    return $this->$attribute;
-  }
-
-  public function save(array $request)
-  {
-    $this->connection = $this->connectionController->connect();
-    $this->connection->beginTransaction();
-    date_default_timezone_set('America/Sao_Paulo');
-    $uuid = UUIDGenerator::uuidv4();
-    $createdAt = date('Y-m-d H:i:s');
-
-    $sql = "INSERT INTO
-      {$this->table}
-      (
-        {$this->columns[1]},
-        {$this->columns[2]},
-        {$this->columns[3]},
-        {$this->columns[4]},
-        {$this->columns[5]}
-      )
-      VALUES(:uuid, :deleted, :active, :product_type, :createdAt);";
-    $stmt = $this->connection->prepare($sql);
-    $stmt->bindValue(":uuid", $uuid, PDO::PARAM_STR);
-    $stmt->bindValue(":deleted", 0, PDO::PARAM_INT);
-    $stmt->bindValue(":active", 1, PDO::PARAM_INT);
-    $stmt->bindValue(":product_type", $request['product_type'], PDO::PARAM_STR);
-    $stmt->bindValue(":createdAt", $createdAt, PDO::PARAM_INT);
-
-    $stmt->execute();
-
-    if ($this->connection->lastInsertId() > 0) {
-      $this->connection->commit();
-      return;
-    }
-
-    $this->connection->rollBack();
-  }
-
-	public function get(array $params): array
+	public function __construct(array $request)
 	{
-		$productsPerPage = 10;
-		$limit = isset($params["limit"]) ? $params["limit"] : $productsPerPage;
-		$page = isset($params["page"]) ? $params["page"] - 1 : 0;
+		date_default_timezone_set('America/Sao_Paulo');
+		$createdAt = date('Y-m-d H:i:s');
+		$updatedAt = date('Y-m-d H:i:s');
 
-		$offset = $page * $limit;
-
-		$productTypes = [];
-
-		$this->connection = $this->connectionController->connect();
-		$sql = "SELECT * FROM {$this->table} LIMIT {$limit} OFFSET {$offset};";
-		$stmt = $this->connection->query($sql);
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			$productTypes[] = $row;
-		}
-
-		return $productTypes;
+		$this->uuid = UUIDGenerator::uuidv4();
+		$this->deleted = 0;
+		$this->active = 1;
+		$this->productType = isset($request['product_type']) ? $request['product_type'] : "";
+		$this->createdAt = $createdAt;
+		$this->updatedAt = $updatedAt;
 	}
 
-  public function update(): void {}
+	public function getUuid(): string
+	{
+		return $this->uuid;
+	}
 
-  public function delete(): void {}
+	public function getDeleted(): bool
+	{
+		return $this->deleted;
+	}
+
+	public function getActive(): bool
+	{
+		return $this->active;
+	}
+
+	public function getProductType(): string
+	{
+		return $this->productType;
+	}
+
+	public function getCreatedAt(): string
+	{
+		return $this->createdAt;
+	}
+
+	public function getUpdatedAt(): string
+	{
+		return $this->updatedAt;
+	}
+
+	public function validateProductType(): bool
+	{
+		$product = [
+			'product_type' => $this->productType,
+		];
+
+		$validationRules = [
+			'product_type' => ['RequiredValidation', 'MaxLengthValidation'],
+		];
+
+		return Validator::validate($product, $validationRules);
+	}
 }
