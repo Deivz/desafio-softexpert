70f5fdcd1b04f30ee4043a0935dab225b02ccd6a
Deivz <carlosd_oliveira@hotmail.com>
2025-02-12 16:47:50 -0300

    TaxRepository readequado

--- src/repositories/TaxRepository.php
+++ src/repositories/TaxRepository.php
@@ -12,27 +12,23 @@ class TaxRepository implements RepositoryInterface
   private PDO $connection;
   private string $table;
   private Tax $tax;
-  private array $tableJoin = [];
 
   public function __construct(PDO $connection, Tax $tax)
   {
     $this->connection = $connection;
     $this->tax = $tax;
     $this->table = $_ENV["TABLE_TAXES"];
-    $this->tableJoin = [
-      $_ENV["TABLE_PRODUCT_TYPES"],
-    ];
   }
 
   public function findByUniqueKey(): int
   {
     $sql = "SELECT id FROM {$this->table}
-      WHERE product_type = :product_type AND deleted = :deleted
+      WHERE name = :name AND deleted = :deleted
       AND uuid != :uuid
       FOR UPDATE";
     $stmt = $this->connection->prepare($sql);
     $stmt->bindValue(':deleted', 0, PDO::PARAM_INT);
-    $stmt->bindValue(':product_type', $this->tax->getProductType(), PDO::PARAM_INT);
+    $stmt->bindValue(':name', $this->tax->getName(), PDO::PARAM_STR);
     $stmt->bindValue(':uuid', $this->tax->getUuid(), PDO::PARAM_STR);
     $stmt->execute();
 
@@ -45,15 +41,15 @@ class TaxRepository implements RepositoryInterface
 
   public function save(): bool
   {
-    $sql = "INSERT INTO {$this->table} (uuid, deleted, active, tax, product_type, created_at)
-      VALUES (:uuid, :deleted, :active, :tax, :product_type, :created_at)";
+    $sql = "INSERT INTO {$this->table} (uuid, deleted, active, name, tax, created_at)
+      VALUES (:uuid, :deleted, :active, :name, :tax, :created_at)";
     $stmt = $this->connection->prepare($sql);
     $stmt->execute([
       ':uuid' => $this->tax->getUuid(),
       ':deleted' => 0,
       ':active' => 1,
+      ':name' => $this->tax->getName(),
       ':tax' => $this->tax->getTax(),
-      ':product_type' => $this->tax->getProductType(),
       ':created_at' => $this->tax->getCreatedAt()
     ]);
 
@@ -68,9 +64,8 @@ class TaxRepository implements RepositoryInterface
 
   public function findAll(int $limit, int $offset): array
   {
-    $sql = "SELECT t.uuid, t.tax, pt.product_type
+    $sql = "SELECT t.uuid, t.name, t.tax
     FROM {$this->table} t
-    INNER JOIN {$this->tableJoin[0]} pt ON (pt.id = t.product_type AND pt.active = 1)
     WHERE t.active = 1
     ORDER BY pt.product_type ASC
     LIMIT :limit OFFSET :offset";
@@ -97,11 +92,11 @@ class TaxRepository implements RepositoryInterface
   public function update(): bool
   {
     $sql = "UPDATE {$this->table}
-    SET tax = :tax, product_type = :product_type, updated_at = :updated_at
+    SET name = :name, tax = :tax, updated_at = :updated_at
     WHERE uuid = :uuid AND active = 1";
     $stmt = $this->connection->prepare($sql);
     $stmt->bindValue(':tax', $this->tax->getTax(), PDO::PARAM_INT);
-    $stmt->bindValue(':product_type', $this->tax->getProductType(), PDO::PARAM_INT);
+    $stmt->bindValue(':name', $this->tax->getName(), PDO::PARAM_STR);
     $stmt->bindValue(':updated_at', $this->tax->getUpdatedAt(), PDO::PARAM_STR);
     $stmt->bindValue(':uuid', $this->tax->getUuid(), PDO::PARAM_STR);
 
