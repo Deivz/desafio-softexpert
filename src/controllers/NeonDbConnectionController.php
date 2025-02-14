5a49addba38112d41ff58524c1ed21f27f2fa2c5
Deivz <carlosd_oliveira@hotmail.com>
2025-02-14 12:23:56 -0300

    Modificação no Controller da conexão

--- src/controllers/NeonDbConnectionController.php
+++ src/controllers/NeonDbConnectionController.php
@@ -5,7 +5,7 @@ namespace Deivz\DesafioSoftexpert\controllers;
 use Deivz\DesafioSoftexpert\interfaces\ConnectionInterface;
 use PDO;
 
-class ConnectionController implements ConnectionInterface
+class NeonDbConnectionController implements ConnectionInterface
 {
   public function __construct(
     private string $host,
@@ -18,14 +18,16 @@ class ConnectionController implements ConnectionInterface
   public function connect(): PDO
   {
     $this->port = (int) $this->port;
+    $dbId = $_ENV['DB_ID'];
 
     return new PDO(sprintf(
-      "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;",
+      "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;options='endpoint=%s'",
       $this->host,
       $this->port,
       $this->name,
       $this->user,
       $this->password,
+      $dbId
     ), $this->user, $this->password, [
       PDO::ATTR_EMULATE_PREPARES => false,
       PDO::ATTR_STRINGIFY_FETCHES => false,
