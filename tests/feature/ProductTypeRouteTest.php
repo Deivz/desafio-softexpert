8036e60dd8830db8354e16463c4ccd6510c2eee3
Deivz <carlosd_oliveira@hotmail.com>
2025-02-14 00:01:20 -0300

    FeatureTest implementado: ProductTypeRoute

---
+++ tests/feature/ProductTypeRouteTest.php
@@ -0,0 +1,111 @@
+<?php
+
+use Deivz\DesafioSoftexpert\controllers\ConnectionController;
+use Deivz\DesafioSoftexpert\controllers\ProductTypeController;
+use Deivz\DesafioSoftexpert\models\ProductType;
+use Deivz\DesafioSoftexpert\repositories\ProductTypeRepository;
+use Deivz\DesafioSoftexpert\services\ProductTypeService;
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+
+class ProductTypeRouteTest extends TestCase
+{
+  private Client $client;
+  private PDO $connection;
+  private $controller;
+  private ProductType $model;
+  private ProductTypeRepository $repository;
+  private ProductTypeService $service;
+
+  protected function setUp(): void
+  {
+
+    if (!file_exists(__DIR__ . '/../../.env')) {
+      throw new Exception('Não foi possível carregar as variáveis de ambiente.');
+    }
+
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+    $dotenv->load();
+
+    $connection = new ConnectionController(
+      $_ENV['DB_HOST'],
+      $_ENV['DB_USER'],
+      $_ENV['DB_PASS'],
+      $_ENV['DB_NAME']
+    );
+
+    $this->connection = $connection->connect();
+
+    $request = [
+      'name' => 'Product Type Test',
+    ];
+
+    $this->model = new ProductType($request);
+    $this->repository = new ProductTypeRepository($this->connection, $this->model);
+    $this->service = new ProductTypeService($this->repository);
+    $this->controller = new ProductTypeController($this->connection, $this->model, $this->service);
+
+    $this->client = new Client([
+      'base_uri' => 'http://localhost:8080',
+      'http_errors' => false
+    ]);
+
+    // uuid será usado pra remover o dado do banco
+    //rollback não vai funcionar por conta da transação do proprio controller
+    $this->model->setUuid('03a934b4-f06f-43f1-b2ce-a5ab93115703');
+  }
+
+  protected function tearDown(): void
+  {
+    $stmt = $this->connection->query(
+      "DELETE FROM {$_ENV["TABLE_PRODUCT_TYPES"]}
+      WHERE uuid = '{$this->model->getUuid()}'"
+    );
+    $stmt->execute();
+  }
+
+  public function test_create_product_type()
+  {
+    // ARRANGE
+    // feito no método setUp 
+
+    // ACT
+    ob_start();
+    $this->controller->create();
+    $output = ob_get_clean();
+
+    // ASSERT
+    $this->assertEquals(201, http_response_code());
+    $this->assertJsonStringEqualsJsonString(
+      json_encode(['mensagem' => 'Tipo de produto cadastrado com sucesso!']),
+      $output
+    );
+
+    // Verificação no banco de dados
+    $stmt = $this->connection->query(
+      "SELECT * FROM {$_ENV["TABLE_PRODUCT_TYPES"]}
+      WHERE name = '{$this->model->getName()}'"
+    );
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->assertNotEmpty($result);
+    $this->assertEquals('Product Type Test', $result['name']);
+  }
+
+  public function test_read_product_type(): void
+  {
+    $response = $this->client->get('/tipos_produto');
+
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
+  // public function testReadProductTypeByUuid(): void
+  // {
+  //   $uuid = '03a934b4-f06f-43f1-b2ce-a5ab93115703';
+  //   $response = $this->client->get("/tipos_produto/$uuid");
+
+  //   $this->assertEquals(200, $response->getStatusCode());
+  //   $data = json_decode($response->getBody(), true);
+  //   $this->assertArrayHasKey('item', $data);
+  // }
+}
